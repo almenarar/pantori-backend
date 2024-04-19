8b83a8134ee500d22916b01202adc15c528b4eef
almenarar <almenarar@gmail.com>
2024-04-19 17:58:28 -0300

    fix(0x prefix on default categories is not needed): 0x is a dart hex convention, not needed on string

--- internal/domains/categories/core/default_categories.go
+++ internal/domains/categories/core/default_categories.go
@@ -3,30 +3,30 @@ package core
 var defaultCategories []Category = []Category{
 	{
 		Name:  "Café da Manhã",
-		Color: "0xFFFCF4A3",
+		Color: "FFFCF4A3",
 	},
 	{
 		Name:  "Almoço",
-		Color: "0xFFFC6A03",
+		Color: "FFFC6A03",
 	},
 	{
 		Name:  "Jantar",
-		Color: "0xFF1338BE",
+		Color: "FF1338BE",
 	},
 	{
 		Name:  "Proteínas",
-		Color: "0xFFFF2800",
+		Color: "FFFF2800",
 	},
 	{
 		Name:  "Vegetais",
-		Color: "0xFF03C04A",
+		Color: "FF03C04A",
 	},
 	{
 		Name:  "Doces",
-		Color: "0xFFFFC1CC",
+		Color: "FFFFC1CC",
 	},
 	{
 		Name:  "Outros",
-		Color: "0xFF92664A",
+		Color: "FF92664A",
 	},
 }
